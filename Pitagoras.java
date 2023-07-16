09cc55d119866d3d1574ed06ebcea1497be0a1f7
Alvaro Jose Cardozo <137445907+Alvaro-C98@users.noreply.github.com>
2023-07-16 03:28:00 -0300

    Pitagoras

---
+++ Pitagoras.java
@@ -0,0 +1,6 @@
+public class Pitagoras {
+    public double calcularHipotenusa(int catetoA, int catetoB) {
+        return Math.sqrt(Math.pow(catetoA,2) + Math.pow(catetoB,2));
+    }
+}
+
---
+++ PitagorasTest.java
@@ -0,0 +1,7 @@
+class PitagorasTest{
+    public static void main(String [] args){
+        Pitagoras calcular = new Pitagoras();
+        //double resultado = calcular.calcularHipotenusa(4,3);
+        System.out.println("Hipotenusa es: "+calcular.calcularHipotenusa(4,3));
+    }
+}
